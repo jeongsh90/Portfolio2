291cbc81fec1083290e4f2908ab4201e2dd0e48b
asdfqwezxcv <149039815+asdfqwezxcv@users.noreply.github.com>
2025-10-29 15:35:25 +0900

    11

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.Portfolio .slide03 .link').on('click', function () {
+        $('#port-03').addClass('on');
+    });
+
+    $('.modal-type1 .close').on('click', function(){
+        $('.modal-type1').removeClass('on')
+    });
+    
+
+    $('#port-03 .menu a').on('click', function (e) {
+        e.preventDefault();
+        let idx = $(this).index();
+        $('#port-03 .menu a').removeClass('on');
+        $('#port-03 .container').removeClass('on');
+        $(this).addClass('on');
+        $('#port-03 .container').eq(idx).addClass('on');
+    });
+
+    $('#port-03').on('click', '.container.on .content', function(){
+        $(this).toggleClass('on');
+    });
+});
