7aab0e8cddbfeef51470af4f8dd564578a4429e8
jeongsh90 <jeongsanghoon@naver.com>
2025-10-29 20:32:06 +0900

    12

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
-    $('.Portfolio .slide03 .link').on('click', function () {
-        $('#port-03').addClass('on');
-    });
+    $('.Portfolio .slide03 .link').on('click', function () { $('#port-03').addClass('on'); });
+    $('.Portfolio .slide04 .link').on('click', function () { $('#port-04').addClass('on'); });
 
-    $('.modal-type1 .close').on('click', function(){
-        $('.modal-type1').removeClass('on')
-    });
+    $('.modal-type1 .close').on('click', function(){ $('.modal-type1').removeClass('on') });
     
 
     $('#port-03 .menu a').on('click', function (e) {
@@ -17,7 +14,5 @@ $(document).ready(function () {
         $('#port-03 .container').eq(idx).addClass('on');
     });
 
-    $('#port-03').on('click', '.container.on .content', function(){
-        $(this).toggleClass('on');
-    });
+    $('#port-03').on('click', '.container.on .content', function(){ $(this).toggleClass('on'); });
 });
